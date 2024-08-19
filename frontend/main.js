4c6fe602622dbfc1c3e6d3f981175144a90033ef
Etowww <etow.engineering@gmail.com>
2024-08-19 15:37:37 -0700

    Basic Frontend Setup

---
+++ frontend/main.js
@@ -0,0 +1,8 @@
+// DBTow Daily Task Manager
+
+const app = document.getElementById("app");
+
+const para =document.createElement("p");
+para.textContent = "Welcome to your productivity app!";
+
+app.appendChild(para);
