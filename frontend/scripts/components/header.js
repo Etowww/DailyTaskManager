d8078f577dff9e4659a790e6025dc1c6130901a5
Etowww <etow.engineering@gmail.com>
2024-10-01 15:11:36 -0700

    Updated component base function

--- frontend/scripts/components/header.js
+++ frontend/scripts/components/header.js
@@ -1,16 +1,18 @@
 // DBTow Daily Task Manager
 
-export function createHeader() {
-    // Create the initial header element
-    const header = document.createElement('header');
-    header.className = 'header';
+export default class Header {
+    constructor(containerId) {
+        this.container = document.getElementById(containerId);
+    }
 
-    // Create the initial h1 element and it's text content
-    const title = document.createElement('h1');
-    title.textContent = 'DBTow Daily Task Manager';
-    
-    // Append the h1 to the header element
-    header.appendChild(title);
+    render() {
+        const header = document.createElement('header');
+        header.className = 'header';
 
-    return header;
+        const title = document.createElement('h1');
+        title.textContent = 'DBTow Daily Task Manager';
+
+        header.appendChild(title);
+        this.container.appendChild(header);
+    }
 }
