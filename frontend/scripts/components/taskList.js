ceebeaab0b1c60435e62ec02ac33bab6694acf3e
Etowww <etow.engineering@gmail.com>
2024-10-01 16:02:23 -0700

    Implement add task functionality in taskList

--- frontend/scripts/components/taskList.js
+++ frontend/scripts/components/taskList.js
@@ -2,16 +2,44 @@
 
 //[Last Updated October 1st]
 
+// Exporting the TaskList class to make it available for import in other modules
 export default class TaskList {
+    // Constructor is called when a new instance of TaskList is created
     constructor(containerId) {
+        // The containerId is used to find the DOM element where the task list will be rendered
+        // 'this.container' stores the reference to the DOM element with the given containerId
         this.container = document.getElementById(containerId);
+
+        // Initialize an empty array to hold the list of tasks
         this.tasks = [];
     }
 
+    // Method to add a new task to the task list
+    addTask(task) {
+        // Push the new task (an object with a title property) into the tasks array
+        this.tasks.push(task);
+
+        // Call the render method to visually update the task list in the DOM
+        this.render();
+    }
+
+    // Method to render the task list on the web page
     render() {
+        // Clear the current content of the container element to avoid duplicating tasks
         this.container.innerHTML = '';
+
+        // Create a new unordered list element to display the list of tasks
         const ul = document.createElement('ul');
-        ul.className = 'task-list';
+        ul.className = 'task-list'; // Add a class name for styling purposes
+
+        // Loop through each task in the tasks array and create a list item for each task 
+        this.tasks.forEach(task => {
+            const li = document.createElement('li');
+            li.textContent = task.title;
+            ul.appendChild(li);
+        });
+
+        // Append the populated ul element to the container in the DOM
         this.container.appendChild(ul);
     }
 }
--- frontend/scripts/main.js
+++ frontend/scripts/main.js
@@ -1,23 +1,51 @@
 // DBTow Daily Task Manager 
 
 //[Last Updated October 1st]
+
+// Log a message to the console to confirm that the script is running
 console.log('main.js is running');
 
 import Header from "./components/header.js";
 import TaskList from "./components/taskList.js";
 
+// Define the main app class that manages the application
 class App {
+    // Constructor is called when a new instance of App is created
     constructor() {
+        // Create a new header instance, linking it to the HTML element with that ID
         this.header = new Header('app-header');
+
+        // Create a new TaskList instance, linking it to the HTML element with that ID
         this.taskList = new TaskList('task-list-container');
+
+        // Initialize the ID counter for new task... this will ensure each task gets a unique id
+        this.nextId = 1;
     }
 
+    // Method to add a new task to the task list
+    addTask(title) {
+        // Create a new task object with a unique ID and the given title
+        const newTask = {
+            id: this.nextId++, // Increment nextId after assigning it
+            title: title
+        };
+
+        // Add the new task to the TaskList instance
+        this.taskList.addTask(newTask);
+    }
+
+    // Initialize the app by rendering the specified components
     init() {
         this.header.render();
         this.taskList.render();
+
     }
 }
 
-// Create and initialize the app
+
+
+// Create a new instance of the App class
 const app = new App();
+
+// Initialize the app
 app.init();
